95b82eb28805f940fefc829bee471c8e9a65bd50
Abhishek Dam <abhishekdam852@gmail.com>
2021-06-01 13:49:53 +0530

    added the side bar navigation and search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     // BEM naming convention (Block Element Modifier methedology)
     <div className="App">
+      {/* Floating App body */}
       <div className="app_body">
         {/* Sidebar */}
+        <Sidebar></Sidebar>
         {/* Chat */}
       </div>
     </div>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import "./Sidebar.css";
+import ChatIcon from "@material-ui/icons/Chat";
+import { DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+
+function Sidebar() {
+  return (
+    //   This whole thing is a side-bar line up :
+    <div className="sidebar">
+      {/* Header of the sidebar */}
+      <div className="sidebar_header">
+        {/* Account Avatar */}
+        <Avatar></Avatar>
+        <div className="sidebar_headerRight">
+          <IconButton>
+            {/* Status Btn */}
+            <DonutLarge></DonutLarge>
+          </IconButton>
+          <IconButton>
+            {/* New chat btn */}
+            <ChatIcon></ChatIcon>
+          </IconButton>
+          <IconButton>
+            {/* More option btn */}
+            <MoreVert></MoreVert>
+          </IconButton>
+        </div>
+      </div>
+      {/* Search box in sidebar */}
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlined></SearchOutlined>
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar_chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
