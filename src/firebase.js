7c674335505a9061fb0e16399b6305e457770168
Abhishek Dam <abhishekdam852@gmail.com>
2021-06-01 12:23:22 +0530

    clean up and added the backgroud

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWVPkSN6z3UA3FJURUPfQWkfXkcSFPGQ0",
+  authDomain: "chat-app-6e25d.firebaseapp.com",
+  projectId: "chat-app-6e25d",
+  storageBucket: "chat-app-6e25d.appspot.com",
+  messagingSenderId: "553167708605",
+  appId: "1:553167708605:web:5805321866e108772af2d4",
+  measurementId: "G-NT8FP9Y6VD",
+};
